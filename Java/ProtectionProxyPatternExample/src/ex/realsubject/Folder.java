394803cb06f807b1b6ac12273f15f6212162b699
Haseeb Ahmed <haseebansari1000@gmail.com>
2021-10-04 00:49:38 +0500

    Proxy Design Pattern Added

---
+++ Java/ProtectionProxyPatternExample/src/ex/realsubject/Folder.java
@@ -0,0 +1,13 @@
+package ex.realsubject;
+
+import ex.subjectInterface.IFolder;
+
+public class Folder implements IFolder
+{
+
+		@Override
+        public void performReadOrWriteOperations()
+        {
+			System.out.println(this.getClass()+" : Performing read or write operation on the folder");          
+        }
+}
